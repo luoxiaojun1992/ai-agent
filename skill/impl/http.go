1cd3d1a3632b3669432bc7e4a69bd5912a1a7fb8
luoxiaojun <luoxiaojun1992@sina.cn>
2025-05-21 17:55:14 +0800

    feat: implement http skill

--- skill/impl/http.go
+++ skill/impl/http.go
@@ -1,8 +1,14 @@
 package impl
 
-import "context"
+import (
+	"context"
+	"errors"
+
+	httpPKG "github.com/luoxiaojun1992/ai-agent/pkg/http"
+)
 
 type Http struct {
+	Client *httpPKG.Client
 }
 
 func (h *Http) GetDescription() string {
@@ -12,5 +18,43 @@ func (h *Http) GetDescription() string {
 
 func (h *Http) Do(ctx context.Context, cmdCtx any, callback func(output any) (any, error)) error {
 	//todo
-	return nil
+	params, isValidParams := cmdCtx.(map[string]any)
+	if !isValidParams {
+		return errors.New("error converting params for http skill")
+	}
+
+	method, hasMethod := params["method"]
+	if !hasMethod {
+		return errors.New("not found method from params")
+	}
+	methodStr, isValidMethod := method.(string)
+	if !isValidMethod {
+		return errors.New("error converting method from params")
+	}
+
+	path, hasPath := params["path"]
+	if !hasPath {
+		return errors.New("not found path from params")
+	}
+	pathStr, isValidPath := path.(string)
+	if !isValidPath {
+		return errors.New("error converting path from params")
+	}
+
+	body, hasBody := params["body"]
+	if !hasBody {
+		return errors.New("not found body from params")
+	}
+	bodyStr, isValidBody := body.(string)
+	if !isValidBody {
+		return errors.New("error converting body from params")
+	}
+
+	//todo parse params
+	res, err := h.Client.SendRequest(methodStr, pathStr, bodyStr, nil, nil)
+	if err != nil {
+		return err
+	}
+	_, err = callback(res)
+	return err
 }
